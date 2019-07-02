74afe2b158b998e50837844eaf1687d303bc8427
Unknown <marivint.yvann@gmail.com>
2019-07-02 11:08:44 +0200

    Dossier configuration

--- 0-config/structure_html.php
+++ 0-config/structure_html.php
@@ -79,18 +79,18 @@ function TplHeader() {  ?>
         <div class="container">
             <div class="navbar-header page-scroll">
                 <!-- <a class="navbar-brand page-scroll" href="#page-top"><img src="img/myhome.png" alt="Lattes theme logo">MyHome</a> -->
-                <a id="logo" class="navbar-brand page-scroll" href="index.php">MyWatch</a>
+                <a id="logo" class="navbar-brand page-scroll" href="index.php">CEMVS</a>
             </div>
             <ul class="nav">
                 <li class="nav-item">
                     <a class="nav-link active mt-1" href="<?php echo URL_HOME ?>index.php">Accueil</a>
-                </li>                      
-                <li class="nav-item">
-                    <a class="nav-link active mt-1" href="<?php echo URL_HOME ?>index.php">Entrainements</a>
-                </li>            
+                </li>                               
                 <?php 
                   if(!empty($_SESSION['utilisateur'])){ ?>
-                    <?php if($_SESSION['utilisateur']['typeutilisateur'] == "administrateur") { ?><li class="nav-item"><a class="nav-link disabled mt-1"  href="<?php echo URL_HOME ?>moncompte.php" >Compétitions</a></li><?php } ?>                  
+                    <?php if($_SESSION['utilisateur']['typeutilisateur'] != "administrateur") { ?><li class="nav-item"><a class="nav-link disabled mt-1"  href="<?php echo URL_HOME ?>entrainement.php" >Entrainements</a></li><?php } ?>                  
+                    <?php if($_SESSION['utilisateur']['typeutilisateur'] == "administrateur") { ?><li class="nav-item"><a class="nav-link disabled mt-1"  href="<?php echo URL_HOME ?>configuration/index.php" >Configuration</a></li><?php } ?>                  
+                    <?php if($_SESSION['utilisateur']['typeutilisateur'] == "utilisateur") { ?><li class="nav-item"><a class="nav-link disabled mt-1"  href="<?php echo URL_HOME ?>moncompte.php" >Compétitions</a></li><?php } ?>                  
+                    <?php if($_SESSION['utilisateur']['typeutilisateur'] == "maitrearme") { ?><li class="nav-item"><a class="nav-link disabled mt-1"  href="<?php echo URL_HOME ?>moncompte.php" >Tireurs</a></li><?php } ?>                  
                 <?php } ?>
                 <li class="nav-item btn-connexion">
                   <?php 
