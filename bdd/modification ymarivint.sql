f160c53ed97fa80c480ecba3f76d34fe8f345b9d
Unknown <marivint.yvann@gmail.com>
2019-07-04 11:37:37 +0200

    BDD

--- bdd/modification ymarivint.sql
+++ bdd/modification ymarivint.sql
@@ -7,3 +7,17 @@ ALTER TABLE `entrainement`
 
 
 ALTER TABLE `participantcompetition` ADD `presence` TINYINT(4) NOT NULL DEFAULT '0' AFTER `id_competition`;
+
+DROP TABLE IF EXISTS `code`;
+CREATE TABLE IF NOT EXISTS `code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `code`
+--
+
+INSERT INTO `code` (`id`, `code`) VALUES
+(1, '267592');
