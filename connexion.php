c6936cfc271f0ab0fb0028e69746ed6da8742c2e
Unknown <gabrielle.ripeau@gmail.com>
2019-07-04 14:16:42 +0200

    maj index

--- connexion.php
+++ connexion.php
@@ -4,26 +4,26 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<?php TplHead(); ?>
-	<body id="page-top"><img src="img/escrime.jpg" alt="img_escrime" height="100%" width="130%" title="Accueil2"/>
+	<body id="page-top">
+
 		<div id="bloc_page">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4">
 						<img src="img/logo/logo-mvds.jpeg" style="float: left;"alt="Logo_page" width="30%" title="Accueil" id="logo"/>
-					</div>
-					<div class="col-md-8" style="margin-top: 2em; ">
+					</div>					<div class="col-md-8" style="margin-top: 2em; ">
 						<center>
 							<h1>Escrime Melun</h1> <br />
 							<h3>Bienvenue</h3>
 						</center>
 					</div>
 				</div>
-				
 			</div>
 		
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-4"></div>
+				
 			    <div id="header-side-login" class="col-md-4"  style="border :2px solid grey; margin-top: 3em;" >
 			        <div id="header-side-panel">
 						<form action="connexion.php?action=connexion" method="post">
